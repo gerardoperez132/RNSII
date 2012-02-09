c7e9fc82109b03012e4b687d0cd3d6c7b54d83fb
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-09 15:33:44 -0430

    Agregando los campos de Funcionalidad.

--- src/ve/gob/cnti/srsi/modelo/Funcionalidad.java
+++ src/ve/gob/cnti/srsi/modelo/Funcionalidad.java
@@ -1,5 +1,50 @@
 package ve.gob.cnti.srsi.modelo;
 
 public class Funcionalidad {
+	private int id;
+	private int id_fun;
+	private int id_si;
+	private String nombre;
+	private String descripcion;
+
+	public int getId() {
+		return id;
+	}
+
+	protected void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId_fun() {
+		return id_fun;
+	}
+
+	public void setId_fun(int id_fun) {
+		this.id_fun = id_fun;
+	}
+
+	public int getId_si() {
+		return id_si;
+	}
+
+	public void setId_si(int id_si) {
+		this.id_si = id_si;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 
 }
