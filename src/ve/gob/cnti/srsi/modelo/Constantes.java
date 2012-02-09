f96b38fcd20747f480046e48073cc4713951be50
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-09 15:56:51 -0430

    Agregando una interfaz de constantes a ser utilizadas durante la implementación de la lógica.

---
+++ src/ve/gob/cnti/srsi/modelo/Constantes.java
@@ -0,0 +1,7 @@
+package ve.gob.cnti.srsi.modelo;
+
+public interface Constantes {
+	public static final int ACTIVO = 0;
+	public static final int MODIFICADO = 1;
+	public static final int ELIMINADO = 2;
+}
