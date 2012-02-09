bfb8c174be519af541f4738956e521b17d4b3e7a
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-09 15:58:19 -0430

    Cambiando el nombre de la interfaz de Constantes a Status.

--- src/ve/gob/cnti/srsi/modelo/Status.java
+++ src/ve/gob/cnti/srsi/modelo/Status.java
@@ -1,6 +1,6 @@
 package ve.gob.cnti.srsi.modelo;
 
-public interface Constantes {
+public interface Status {
 	public static final int ACTIVO = 0;
 	public static final int MODIFICADO = 1;
 	public static final int ELIMINADO = 2;
