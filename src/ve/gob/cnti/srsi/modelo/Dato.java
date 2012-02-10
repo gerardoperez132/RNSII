9594f4fae9014aa811249a840a892202c0504293
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-10 10:31:10 -0430

    Agregando el modelo de Dato que describe una relación recursiva consigo mismo indicando que tendrá padre si es de tipo compuesto y ningún padre si es de tipo simple. Los tipos de datos estarán en otra tabla que tendrá un discriminante para listar los datos en la vista.

---
+++ src/ve/gob/cnti/srsi/modelo/Dato.java
@@ -0,0 +1,16 @@
+package ve.gob.cnti.srsi.modelo;
+
+import java.util.Date;
+
+public class Dato {
+	private int id;
+	private int dato;
+	private int entrada_salida;
+	private int id_padre;
+	private String nombre;
+	private String descripcion;
+	private int status;
+	private Date fecha_creado;
+	private Date fecha_modificado;
+
+}
