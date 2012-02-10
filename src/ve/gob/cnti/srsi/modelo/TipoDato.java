f9f014ce21be9876d76874b1d4083e3978ec3671
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-10 10:34:40 -0430

    Agregando modelo para los Tipos de Datos. Generalmente todos los tipos de datos que son simples, como String, Decimal, Integer, etc.; y los datos que son compuestos, como tipo Lista y Evento.

---
+++ src/ve/gob/cnti/srsi/modelo/TipoDato.java
@@ -0,0 +1,16 @@
+package ve.gob.cnti.srsi.modelo;
+
+import java.util.Date;
+
+public class TipoDato {
+
+	private int id;
+	private int tipo_dato;
+	private int clase_dato; // Simple o compuesto
+	private String nombre;
+	private String descripcion;
+	private int status;
+	private Date fecha_creado;
+	private Date fecha_modificado;
+
+}
