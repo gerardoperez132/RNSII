0db9c70f18303cd563819ca5ec68129813a2218e
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-10 16:38:06 -0430

    Agregando métodos.

--- src/ve/gob/cnti/srsi/modelo/Suscrito.java
+++ src/ve/gob/cnti/srsi/modelo/Suscrito.java
@@ -12,4 +12,68 @@ public class Suscrito {
 	private Date fecha_creado;
 	private Date fecha_modificado;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId_suscrito() {
+		return id_suscrito;
+	}
+
+	public void setId_suscrito(int id_suscrito) {
+		this.id_suscrito = id_suscrito;
+	}
+
+	public int getId_ente() {
+		return id_ente;
+	}
+
+	public void setId_ente(int id_ente) {
+		this.id_ente = id_ente;
+	}
+
+	public int getId_servicio_informacion() {
+		return id_servicio_informacion;
+	}
+
+	public void setId_servicio_informacion(int id_servicio_informacion) {
+		this.id_servicio_informacion = id_servicio_informacion;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Date getFecha_creado() {
+		return fecha_creado;
+	}
+
+	public void setFecha_creado(Date fecha_creado) {
+		this.fecha_creado = fecha_creado;
+	}
+
+	public Date getFecha_modificado() {
+		return fecha_modificado;
+	}
+
+	public void setFecha_modificado(Date fecha_modificado) {
+		this.fecha_modificado = fecha_modificado;
+	}
+
+	@Override
+	public String toString() {
+		return "Suscrito [id=" + id + ", id_suscrito=" + id_suscrito
+				+ ", id_ente=" + id_ente + ", id_servicio_informacion="
+				+ id_servicio_informacion + ", status=" + status
+				+ ", fecha_creado=" + fecha_creado + ", fecha_modificado="
+				+ fecha_modificado + "]";
+	}
 }
