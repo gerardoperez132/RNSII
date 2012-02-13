3da5a6236049bfcac8c12d8d8623b2ab66592a3a
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-13 16:33:13 -0430

    Agregando una especie de esquema DAO estándar.

--- src/ve/gob/cnti/srsi/dao/DAO.java
+++ src/ve/gob/cnti/srsi/dao/DAO.java
@@ -1,13 +1,30 @@
 package ve.gob.cnti.srsi.dao;
 
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class DAO {
 
-	public static Session session;
-	public static Transaction transaction;
+	private static Session session;
+	private static Transaction transaction;
+
+	public static Session getSession() {
+		return session;
+	}
+
+	public static void setSession(Session session) {
+		DAO.session = session;
+	}
+
+	public static Transaction getTransaction() {
+		return transaction;
+	}
+
+	public static void setTransaction(Transaction transaction) {
+		DAO.transaction = transaction;
+	}
 
 	public static void startConnection() {
 		session = HibernateUtils.getSessionFactory().openSession();
@@ -29,9 +46,10 @@ public class DAO {
 		long id = 0;
 		try {
 			startConnection();
-			// Obtener el id mayor de la columna dada.
-			// id = SELECT MAX(column) FROM table;
-			// id++;
+			Query query = session.createSQLQuery("SELECT MAX(" + column
+					+ ") FROM " + table);
+			if (query.uniqueResult() != null)
+				id = Long.parseLong(String.valueOf(query.uniqueResult())) + 1;
 		} catch (HibernateException he) {
 			handleException(he);
 			throw he;
