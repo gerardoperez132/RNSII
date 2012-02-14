3df72e00049835c29e01fdc0f2c621c12f3513b9
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-14 10:11:44 -0430

    Agregando descripción de la clase, atributos y anotaciones. Esta tabla tendrá un nombre en singular porque son los niveles de seguridad; se llamará ahora "seguridad" simplemente. Sujeto a cambios.

---
+++ src/ve/gob/cnti/srsi/modelo/Seguridad.java
@@ -0,0 +1,85 @@
+package ve.gob.cnti.srsi.modelo;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Especificas los niveles de seguridad de un servicio de información.
+ * 
+ * @author Richard Ricciardelli
+ * 
+ */
+@Entity
+@Table(name = "seguridad")
+public class Seguridad {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private long id_seguridad;
+	private String nombre;
+	private int status;
+	private Date fecha_creado;
+	private Date fecha_modificado;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getId_seguridad() {
+		return id_seguridad;
+	}
+
+	public void setId_seguridad(long id_seguridad) {
+		this.id_seguridad = id_seguridad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Date getFecha_creado() {
+		return fecha_creado;
+	}
+
+	public void setFecha_creado(Date fecha_creado) {
+		this.fecha_creado = fecha_creado;
+	}
+
+	public Date getFecha_modificado() {
+		return fecha_modificado;
+	}
+
+	public void setFecha_modificado(Date fecha_modificado) {
+		this.fecha_modificado = fecha_modificado;
+	}
+
+	@Override
+	public String toString() {
+		return "Seguridad [id=" + id + ", id_seguridad=" + id_seguridad
+				+ ", nombre=" + nombre + ", status=" + status
+				+ ", fecha_creado=" + fecha_creado + ", fecha_modificado="
+				+ fecha_modificado + "]";
+	}
+}
