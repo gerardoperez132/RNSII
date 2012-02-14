ed31ca1b0f069240aa5c2d422422062ce8c678d2
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-14 10:26:43 -0430

    Cambiando el nombre del modelo de URL y el nombre que tendrá en la base de datos.

--- src/ve/gob/cnti/srsi/modelo/URL.java
+++ src/ve/gob/cnti/srsi/modelo/URL.java
@@ -9,61 +9,74 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="usuarios")
-public class Url {
+@Table(name = "url")
+public class URL {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private int id_url;
-	private int id_ente;	
+	private int id_ente;
 	private String url;
 	private int status;
 	private Date fecha_creado;
 	private Date fecha_modificado;
-	
-	public Url() {	
+
+	public URL() {
 	}
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
-	}	
+	}
+
 	public int getId_url() {
 		return id_url;
 	}
+
 	public void setId_url(int id_url) {
 		this.id_url = id_url;
 	}
+
 	public int getId_ente() {
 		return id_ente;
 	}
+
 	public void setId_ente(int id_ente) {
 		this.id_ente = id_ente;
 	}
+
 	public String getUrl() {
 		return url;
 	}
+
 	public void setUrl(String url) {
 		this.url = url;
 	}
+
 	public int getStatus() {
 		return status;
 	}
+
 	public void setStatus(int status) {
 		this.status = status;
 	}
+
 	public Date getFecha_creado() {
 		return fecha_creado;
 	}
+
 	public void setFecha_creado(Date fecha_creado) {
 		this.fecha_creado = fecha_creado;
 	}
+
 	public Date getFecha_modificado() {
 		return fecha_modificado;
 	}
+
 	public void setFecha_modificado(Date fecha_modificado) {
 		this.fecha_modificado = fecha_modificado;
 	}
@@ -74,5 +87,4 @@ public class Url {
 				+ ", url=" + url + ", status=" + status + ", fecha_creado="
 				+ fecha_creado + ", fecha_modificado=" + fecha_modificado + "]";
 	}
-	
 }
