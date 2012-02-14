8d64c52c83a4a92d2ad2e88d8153960ac8c2e1ef
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-14 11:27:01 -0430

    Preparando el controlador para el registro.

--- src/ve/gob/cnti/srsi/dao/ServicioInformacionDAO.java
+++ src/ve/gob/cnti/srsi/dao/ServicioInformacionDAO.java
@@ -1,37 +1,34 @@
 package ve.gob.cnti.srsi.dao;
 
-import java.util.Date;
-
-import ve.gob.cnti.srsi.modelo.Estado;
-
 /**
  * Controlador del Servicio de Información.
  * 
  * @author Richard Ricciardelli
  * 
  */
-public class ServicioInformacionDAO extends DAO {
-	// Prueba conceptual con modelo de Estado.
-	private static Estado estado;
+public class ServicioInformacionDAO extends DAO implements CRUD {
 
-	public static void main(String args[]) {
-		estado = new Estado();
-		long id = getNextId("estados", "id_estado");
-		Date fecha = new Date();
-		/** Colocar el id correspondiente en base al algoritmo. */
-		estado.setId_estado(id != 0 ? id : 1);
-		estado.setNombre("En desarrollo");
-		estado.setStatus(Status.ACTIVO);
-		estado.setFecha_creado(fecha);
-		estado.setFecha_modificado(fecha);
-		createEstado();
+	@Override
+	public void create(Object model) {
+		// TODO Auto-generated method stub
+
 	}
 
-	private static void createEstado() {
-		startConnection();
-		getSession().save(estado);
-		getTransaction().commit();
-		closeConnection();
+	@Override
+	public void read() {
 		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+
 	}
 }
