bec019d0efbaf707035a2ff91bda12a4136bedc2
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-14 11:07:32 -0430

    Agregando los identificadores en formato long.

--- src/ve/gob/cnti/srsi/modelo/Ente.java
+++ src/ve/gob/cnti/srsi/modelo/Ente.java
@@ -1,13 +1,5 @@
 package ve.gob.cnti.srsi.modelo;
 
-/****
- *
- * @author joaquin
- * 
- * Clase modelo con los atributos de la tabla entes
- *
- */
-
 import java.util.Date;
 
 import javax.persistence.Entity;
@@ -16,81 +8,102 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * Clase modelo con los atributos de la tabla entes.
+ * 
+ * @author Joaquín Pereira
+ */
 @Entity
-@Table(name="entes")
+@Table(name = "entes")
 public class Ente {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
-	private int id_ente;
-	private int id_padre;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private long id_ente;
+	private long id_padre;
 	private String nombre;
 	private String rif;
 	private String direccion;
 	private int status;
 	private Date fecha_creado;
 	private Date fecha_modificado;
-		
+
 	public Ente() {
-		super();
 	}
-	
-	public int getId() {
+
+	public long getId() {
 		return id;
 	}
-	public void setId(int id) {
+
+	public void setId(long id) {
 		this.id = id;
 	}
-	public int getId_ente() {
+
+	public long getId_ente() {
 		return id_ente;
 	}
-	public void setId_ente(int id_ente) {
+
+	public void setId_ente(long id_ente) {
 		this.id_ente = id_ente;
 	}
-	public int getId_padre() {
+
+	public long getId_padre() {
 		return id_padre;
 	}
-	public void setId_padre(int id_padre) {
+
+	public void setId_padre(long id_padre) {
 		this.id_padre = id_padre;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getRif() {
 		return rif;
 	}
+
 	public void setRif(String rif) {
 		this.rif = rif;
 	}
+
 	public String getDireccion() {
 		return direccion;
 	}
+
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
+
 	public int getStatus() {
 		return status;
 	}
+
 	public void setStatus(int status) {
 		this.status = status;
 	}
+
 	public Date getFecha_creado() {
 		return fecha_creado;
 	}
+
 	public void setFecha_creado(Date fecha_creado) {
 		this.fecha_creado = fecha_creado;
 	}
+
 	public Date getFecha_modificado() {
 		return fecha_modificado;
 	}
+
 	public void setFecha_modificado(Date fecha_modificado) {
 		this.fecha_modificado = fecha_modificado;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Ente [id=" + id + ", id_ente=" + id_ente + ", id_padre="
@@ -98,6 +111,5 @@ public class Ente {
 				+ ", direccion=" + direccion + ", status=" + status
 				+ ", fecha_creado=" + fecha_creado + ", fecha_modificado="
 				+ fecha_modificado + "]";
-	}	
-	
+	}
 }
