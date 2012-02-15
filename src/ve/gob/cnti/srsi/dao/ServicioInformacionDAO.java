2ee7391a56c06bd3336388f3c1141e79fce7d487
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-15 11:36:17 -0430

    Eliminando la lógica de pruebas.

--- src/ve/gob/cnti/srsi/dao/ServicioInformacionDAO.java
+++ src/ve/gob/cnti/srsi/dao/ServicioInformacionDAO.java
@@ -1,9 +1,5 @@
 package ve.gob.cnti.srsi.dao;
 
-import java.util.Date;
-
-import ve.gob.cnti.srsi.modelo.Estado;
-
 /**
  * Controlador del Servicio de Información.
  * 
@@ -11,16 +7,4 @@ import ve.gob.cnti.srsi.modelo.Estado;
  * 
  */
 public class ServicioInformacionDAO extends DAO {
-
-	public static void main(String args[]) {
-		// Probando actualizaciones
-		Date fecha = new Date();
-
-		Estado estado = new Estado();
-		estado.setId_estado(getNextId("estados", "id_estado"));
-		estado.setNombre("Implementado");
-		estado.setStatus(Status.ACTIVO);
-		estado.setFecha_creado(fecha);
-		estado.setFecha_modificado(fecha);
-	}
 }
