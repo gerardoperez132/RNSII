ec78cb5e735259cd7ae822f22cdbf7f1eb1d9526
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-15 13:33:57 -0430

    Agregando descripciones a las definiciones de métodos en la interfaz CRUD.

--- src/ve/gob/cnti/srsi/dao/CRUD.java
+++ src/ve/gob/cnti/srsi/dao/CRUD.java
@@ -8,17 +8,63 @@ public interface CRUD {
 	 * Permite guardar un registro en la base de datos.
 	 * 
 	 * @param model
-	 *            Es el modelo o tabla en el cual se realizará el registro.
+	 *            Es la clase, modelo o tabla en el cual se realizará el
+	 *            registro.
 	 */
 	public void create(Object model);
 
+	/**
+	 * Permite obtener el registro activo especificado por id del modelo dado.
+	 * 
+	 * @param model
+	 *            Es la clase, modelo o tabla en la cual se realizará la
+	 *            consulta.
+	 * @param id
+	 *            Es el id del registro a consultar.
+	 * @return El objeto modelo con sus atributos.
+	 */
 	public Object read(Object model, long id);
 
+	/**
+	 * Permite obtener todos los registros activos del modelo dado.
+	 * 
+	 * @param model
+	 *            Es la clase, modelo o tabla en la cual se realizará la
+	 *            consulta.
+	 * @return Una lista de objetos del modelo dado con sus atributos.
+	 */
 	public ArrayList<Object> read(Object model);
 
-	public void update(Object model, String table, String column, long id);
+	/**
+	 * Permite modificar el registro especificado por id del modelo dado.
+	 * 
+	 * @param model
+	 *            Es la clase, modelo o tabla en la cual se realizará la
+	 *            consulta.
+	 * @param id
+	 *            Es el id del registro a modificar.
+	 */
+	public void update(Object model, long id);
 
-	public void delete();
+	/**
+	 * Permite eliminar el registro especificado por id del modelo dado.
+	 * 
+	 * @param model
+	 *            Es la clase, modelo o tabla en la cual se realizará la
+	 *            consulta.
+	 * @param id
+	 *            Es el id del registro a eliminar.
+	 */
+	public void delete(Object model, long id);
 
+	/**
+	 * Permite obtener el id lógico correspondiente al modelo dado para poder
+	 * crear un nuevo registro.
+	 * 
+	 * @param model
+	 *            Es la clase, modelo o tabla en la cual se realizará la
+	 *            consulta.
+	 * @return id Es el id lógico correspondiente.
+	 */
 	public long getNextId(Object model);
 }
