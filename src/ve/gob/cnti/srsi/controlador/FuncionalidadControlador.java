fa114ad6094bd2bda23af0aa550838508fc959c6
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-22 15:37:08 -0430

    Creando la clase controladora del registro de funcionalidades.

---
+++ src/ve/gob/cnti/srsi/controlador/FuncionalidadControlador.java
@@ -0,0 +1,12 @@
+package ve.gob.cnti.srsi.controlador;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class FuncionalidadControlador extends ActionSupport {
+
+	public String prepararRegistroFuncionalidad() {
+		return SUCCESS;
+
+	}
+
+}
