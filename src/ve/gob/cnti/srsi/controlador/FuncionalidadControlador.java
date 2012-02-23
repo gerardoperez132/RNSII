ea5685b965b965e1204aeaade9c0e81bd812ebbf
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-23 10:36:33 -0430

    Generando setters y getters. Consultando las funcionalidades ya cargadas para un determinado servicio de información.

--- src/ve/gob/cnti/srsi/controlador/FuncionalidadControlador.java
+++ src/ve/gob/cnti/srsi/controlador/FuncionalidadControlador.java
@@ -1,12 +1,40 @@
 package ve.gob.cnti.srsi.controlador;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ve.gob.cnti.srsi.dao.DAO;
+import ve.gob.cnti.srsi.modelo.Funcionalidad;
+
 import com.opensymphony.xwork2.ActionSupport;
 
+@SuppressWarnings("serial")
 public class FuncionalidadControlador extends ActionSupport {
 
-	public String prepararRegistroFuncionalidad() {
-		return SUCCESS;
+	private List<Funcionalidad> funcionalidades = new ArrayList<Funcionalidad>();
+	private DAO dao = new DAO();
 
+	@SuppressWarnings("unchecked")
+	public String prepararRegistroFuncionalidad() {
+		Funcionalidad funcionalidad = new Funcionalidad();
+		funcionalidades = (List<Funcionalidad>) dao.read(funcionalidad);
+
+		return SUCCESS;
 	}
 
+	public List<Funcionalidad> getFuncionalidades() {
+		return funcionalidades;
+	}
+
+	public void setFuncionalidades(List<Funcionalidad> funcionalidades) {
+		this.funcionalidades = funcionalidades;
+	}
+
+	public DAO getDao() {
+		return dao;
+	}
+
+	public void setDao(DAO dao) {
+		this.dao = dao;
+	}
 }
