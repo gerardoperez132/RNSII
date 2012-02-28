d2ded3914f8288138d5a87220b50bed823306125
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-28 17:18:42 -0430

    Agregando una interfaz de constantes para manejar varias sub-interfaces que se utilizarán en la aplicación.

---
+++ src/ve/gob/cnti/srsi/dao/Constants.java
@@ -0,0 +1,37 @@
+package ve.gob.cnti.srsi.dao;
+
+/**
+ * Interfaz de constantes del Sistema de Registro de Servicio de Información.
+ * 
+ * @author Richard Ricciardelli
+ * 
+ */
+public interface Constants {
+	/**
+	 * Interfaz con los estados de un sistema de información.
+	 * 
+	 * @author Richard Ricciardelli
+	 * 
+	 */
+	public interface Status {
+		/** Estado ACTIVO se muestra en la vista. */
+		public static final int ACTIVO = 0;
+		/** Estado MODIFICADO no se muestra en la vista. */
+		public static final int MODIFICADO = 1;
+		/** Estado ELIMINADO no se muestra en la vista. */
+		public static final int ELIMINADO = 2;
+	}
+
+	/**
+	 * Interfaz con los tipos de documentos de los aspectos legales.
+	 * 
+	 * @author Richard Ricciardelli
+	 * 
+	 */
+	public interface TipoDocumento {
+		/** Tipo de documento legal. */
+		public static final int LEGAL = 0;
+		/** Tipo de documento SLA. */
+		public static final int SLA = 1;
+	}
+}
