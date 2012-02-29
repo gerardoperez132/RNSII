b6ff2a15cd0acbb5efd16f965133927960211073
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-29 10:52:08 -0430

    Agregando interfaz para el formulario, tiene el método de prepararFormulario() el cual será sobreescrito por cada clase formulario de cada modelo.

---
+++ src/ve/gob/cnti/srsi/controlador/Formulario.java
@@ -0,0 +1,16 @@
+package ve.gob.cnti.srsi.controlador;
+
+/**
+ * Interfaz con métodos para implementar en cada formulario.
+ * 
+ * @author Richard Ricciardelli
+ * 
+ */
+public interface Formulario {
+	/**
+	 * Prepara el formulario del modelo dado.
+	 * 
+	 * @return "success"
+	 */
+	public String prepararFormulario();
+}
