a8bc020dabdcfc296be88e6f21694a831000f081
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-02-29 15:50:54 -0430

    Agregando objetos.

--- src/ve/gob/cnti/srsi/controlador/FuncionalidadControlador.java
+++ src/ve/gob/cnti/srsi/controlador/FuncionalidadControlador.java
@@ -1,6 +1,29 @@
 package ve.gob.cnti.srsi.controlador;
 
+import ve.gob.cnti.srsi.modelo.EntradaSalida;
+import ve.gob.cnti.srsi.modelo.Funcionalidad;
+
 @SuppressWarnings("serial")
-public class FuncionalidadControlador {
+public class FuncionalidadControlador extends FuncionalidadFormulario {
+	private Funcionalidad funcionalidad;
+	private EntradaSalida entrada;
+	private EntradaSalida salida;
+
+	public String registrarFuncionalidad() {
+		funcionalidad.setId(100);
+		System.out.println(funcionalidad.getId());
+		System.out.println(funcionalidad.getNombre());
+		System.out.println(funcionalidad.getDescripcion());
+		return SUCCESS;
+
+	}
+
+	public Funcionalidad getFuncionalidad() {
+		return funcionalidad;
+	}
+
+	public void setFuncionalidad(Funcionalidad funcionalidad) {
+		this.funcionalidad = funcionalidad;
+	}
 
 }
