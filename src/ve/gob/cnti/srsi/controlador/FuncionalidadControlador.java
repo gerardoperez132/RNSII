4c2b469a5816ce36522790a6310adb881568af70
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-03-01 13:20:27 -0430

    Agregando tipos de datos.

--- src/ve/gob/cnti/srsi/controlador/FuncionalidadControlador.java
+++ src/ve/gob/cnti/srsi/controlador/FuncionalidadControlador.java
@@ -1,10 +1,17 @@
 package ve.gob.cnti.srsi.controlador;
 
+import java.util.List;
+
+import ve.gob.cnti.srsi.dao.DAO;
 import ve.gob.cnti.srsi.modelo.EntradaSalida;
 import ve.gob.cnti.srsi.modelo.Funcionalidad;
+import ve.gob.cnti.srsi.modelo.TipoDato;
 
 @SuppressWarnings("serial")
-public class FuncionalidadControlador extends FuncionalidadFormulario {
+public class FuncionalidadControlador extends DAO implements Formulario {
+
+	private List<Funcionalidad> funcionalidades;
+	private List<TipoDato> tipoDatos;
 	private Funcionalidad funcionalidad;
 	private EntradaSalida entrada;
 	private EntradaSalida salida;
@@ -13,7 +20,36 @@ public class FuncionalidadControlador extends FuncionalidadFormulario {
 		System.out.println(funcionalidad.getNombre());
 		System.out.println(funcionalidad.getDescripcion());
 		return SUCCESS;
+	}
 
+	@SuppressWarnings("unchecked")
+	@Override
+	public String prepararFormulario() {
+		Funcionalidad funcionalidad = new Funcionalidad();
+		TipoDato tipoDato = new TipoDato();
+		funcionalidades = (List<Funcionalidad>) read(funcionalidad);
+		tipoDatos = (List<TipoDato>) read(tipoDato);
+
+		if (funcionalidades.isEmpty())
+			addFieldError("funcionalidades",
+					"No hay funcionalidades asociadas a este servicio de información.");
+		return SUCCESS;
+	}
+
+	public List<TipoDato> getTipoDatos() {
+		return tipoDatos;
+	}
+
+	public void setTipoDatos(List<TipoDato> tipoDatos) {
+		this.tipoDatos = tipoDatos;
+	}
+
+	public List<Funcionalidad> getFuncionalidades() {
+		return funcionalidades;
+	}
+
+	public void setFuncionalidades(List<Funcionalidad> funcionalidades) {
+		this.funcionalidades = funcionalidades;
 	}
 
 	public EntradaSalida getEntrada() {
@@ -39,5 +75,4 @@ public class FuncionalidadControlador extends FuncionalidadFormulario {
 	public void setFuncionalidad(Funcionalidad funcionalidad) {
 		this.funcionalidad = funcionalidad;
 	}
-
 }
