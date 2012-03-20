b23a51f5d25fba9d4fa1fd71b2937126cbaa01f0
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-03-20 08:47:20 -0430

    Arreglando una asignación.

--- src/ve/gob/cnti/srsi/controlador/LoginControlador.java
+++ src/ve/gob/cnti/srsi/controlador/LoginControlador.java
@@ -6,33 +6,35 @@ import ve.gob.cnti.srsi.modelo.Usuario;
 
 @SuppressWarnings("serial")
 public class LoginControlador extends DAO {
-		
+
 	private String correo;
-    private String password;
-    private Usuario usuario = new Usuario();
-    private Correo user_correo = new Correo();
-		
-	public String autenticarUsuario(){
-		
-		user_correo = (Correo) read(correo, correo); 
-		if(user_correo == null || user_correo.getId_usuario()==0){
-			addFieldError("correo","Correo no existe");
+	private String password;
+	private Usuario usuario = new Usuario();
+	private Correo user_correo = new Correo();
+
+	public String autenticarUsuario() {
+
+		// user_correo = (Correo) read(correo, correo);
+		user_correo = getUserEmail(correo);
+		if (user_correo == null || user_correo.getId_usuario() == 0) {
+			addFieldError("correo", "Correo no existe");
 			return INPUT;
-		}else{
+		} else {
 			usuario = (Usuario) read(usuario, user_correo.getId_usuario());
-			if(usuario==null){
-				addFieldError("correo","Ha ocurrido un problema recuperando sus datos!!!");
+			if (usuario == null) {
+				addFieldError("correo",
+						"Ha ocurrido un problema recuperando sus datos!!!");
 				return INPUT;
-			}else if(!usuario.getClave().contains(password)){
-				addFieldError("password","La clave no coincide con el correo");
+			} else if (!usuario.getClave().contains(password)) {
+				addFieldError("password", "La clave no coincide con el correo");
 				return INPUT;
-			}else{
+			} else {
 				return SUCCESS;
 			}
-		}		
+		}
 	}
-	
-	public String mostrarLogin(){
+
+	public String mostrarLogin() {
 		return SUCCESS;
 	}
 
@@ -43,7 +45,7 @@ public class LoginControlador extends DAO {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public String getCorreo() {
 		return correo;
 	}
