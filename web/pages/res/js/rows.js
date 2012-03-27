e93d1de5cfad10a89db77fffaf61d14d005d9202
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-03-27 15:16:18 -0430

    Agregando una función JavaScript que permitirá agregar otro documento en la misma vista dependiendo de cuántos se quieran subir.

---
+++ web/pages/res/js/rows.js
@@ -0,0 +1,28 @@
+var File = function() {
+	var nameLabel;
+	var fileLabel;
+	function createRow() {
+		// return
+		// '<s:fielderror><s:param>name</s:param></s:fielderror><s:textfield
+		// name="name" labelposition="top" /><h5 class="formulario"><s:text
+		// name="documento.file"></s:text></h5><s:fielderror><s:param>file</s:param></s:fielderror><s:file
+		// name="file" />';
+		// return '<input type="submit" value="Submit">';
+
+		form = '<h5 class="formulario">' + nameLabel + '</h5>'
+				+ '<input type="text" name="name" id="name">'
+				+ '<h5 class="formulario">' + fileLabel
+				+ '</h5><input type="file" name="file" value="" id="file"><hr>';
+		return form;
+	}
+	return {
+		prepare : function(name, file) {
+			nameLabel = name;
+			fileLabel = file;
+		},
+		addRow : function() {
+			$("#addRow").before(createRow());
+			return false;
+		}
+	};
+}();
