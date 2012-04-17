048b73f5032d91f4518a79cb146d28b277ebb1c8
Joaquín Pereira <pereira.joaquin@gmail.com>
2012-04-17 15:12:35 -0430

    archivo jquery con sus selectores listos

--- web/pages/res/js/datos.js
+++ web/pages/res/js/datos.js
@@ -1,11 +1,56 @@
 $(document).ready(function(){
-	$("#tipoDato").change(function (e){		
-		if($("#tipoDato").val()==2){
+	$("#tipoDato").change(function (e){	
+		if($("#tipoDato").val()== -1){
+			$('#string').attr('style', 'visibility: hidden; position:fixed;');
+			$('#integer').attr('style', 'visibility: hidden; position:fixed;');
+			$('#decimal').attr('style', 'visibility: hidden; position:fixed;');
+			$('#boolean').attr('style', 'visibility: hidden; position:fixed;');
+			$('#date').attr('style', 'visibility: hidden; position:fixed;');
+			$('#time').attr('style', 'visibility: hidden; position:fixed;');
+		}else if($("#tipoDato").val()==2){
 			$('#string').attr('style', 'visibility: visible; position:relative;');
 			$('#integer').attr('style', 'visibility: hidden; position:fixed;');
+			$('#decimal').attr('style', 'visibility: hidden; position:fixed;');
+			$('#boolean').attr('style', 'visibility: hidden; position:fixed;');
+			$('#date').attr('style', 'visibility: hidden; position:fixed;');
+			$('#time').attr('style', 'visibility: hidden; position:fixed;');
 		}else if($("#tipoDato").val()==3){
 			$('#string').attr('style', 'visibility: hidden; position:fixed;');
 			$('#integer').attr('style', 'visibility: visible; position:relative;');
+			$('#decimal').attr('style', 'visibility: hidden; position:fixed;');
+			$('#boolean').attr('style', 'visibility: hidden; position:fixed;');
+			$('#date').attr('style', 'visibility: hidden; position:fixed;');
+			$('#time').attr('style', 'visibility: hidden; position:fixed;');
+		}else if($("#tipoDato").val()==4){
+			$('#string').attr('style', 'visibility: hidden; position:fixed;');
+			$('#integer').attr('style', 'visibility: hidden; position:fixed;');
+			$('#decimal').attr('style', 'visibility: visible; position:relative;');
+			$('#boolean').attr('style', 'visibility: hidden; position:fixed;');
+			$('#date').attr('style', 'visibility: hidden; position:fixed;');
+			$('#time').attr('style', 'visibility: hidden; position:fixed;');
+		}else if($("#tipoDato").val()==5){
+			$('#string').attr('style', 'visibility: hidden; position:fixed;');
+			$('#integer').attr('style', 'visibility: hidden; position:fixed;');
+			$('#decimal').attr('style', 'visibility: hidden; position:fixed;');
+			$('#boolean').attr('style', 'visibility: visible; position:relative;');
+			$('#date').attr('style', 'visibility: hidden; position:fixed;');
+			$('#time').attr('style', 'visibility: hidden; position:fixed;');
+		}else if($("#tipoDato").val()==6){
+			$('#string').attr('style', 'visibility: hidden; position:fixed;');
+			$('#integer').attr('style', 'visibility: hidden; position:fixed;');
+			$('#decimal').attr('style', 'visibility: hidden; position:fixed;');
+			$('#boolean').attr('style', 'visibility: hidden; position:fixed;');
+			$('#date').attr('style', 'visibility: visible; position:relative;');
+			$('#time').attr('style', 'visibility: hidden; position:fixed;');
+		}else if($("#tipoDato").val()==7){
+			$('#string').attr('style', 'visibility: hidden; position:fixed;');
+			$('#integer').attr('style', 'visibility: hidden; position:fixed;');
+			$('#decimal').attr('style', 'visibility: hidden; position:fixed;');
+			$('#boolean').attr('style', 'visibility: hidden; position:fixed;');
+			$('#date').attr('style', 'visibility: hidden; position:fixed;');
+			$('#time').attr('style', 'visibility: visible; position:relative;');
 		}
+		
+		
 	});
 });
