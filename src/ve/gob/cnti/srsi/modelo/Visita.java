b20fb02ee565b77f25a2ba71ddd6943b1f82aee1
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-05-15 09:53:20 -0430

    Agregando formato y descripción como comentario a la clase.

--- src/ve/gob/cnti/srsi/modelo/Visita.java
+++ src/ve/gob/cnti/srsi/modelo/Visita.java
@@ -24,15 +24,15 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
- * Aspectos legales del servicio de información.
+ * Visitas que recibe un servicio de información particular.
  * 
  * @author Joaquín Pereira
- * 
+ * @see ServicioInformacion
  */
 @Entity
 @Table(name = "visitas")
 public class Visita {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
@@ -41,42 +41,54 @@ public class Visita {
 	private int status;
 	private Date fecha_creado;
 	private Date fecha_modificado;
-	
-	public Visita(){}
-	
+
+	public Visita() {
+	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public long getId_visita() {
 		return id_visita;
 	}
+
 	public void setId_visita(long id_visita) {
 		this.id_visita = id_visita;
 	}
+
 	public long getId_servicio_informacion() {
 		return id_servicio_informacion;
 	}
+
 	public void setId_servicio_informacion(long id_servicio_informacion) {
 		this.id_servicio_informacion = id_servicio_informacion;
-	}	
+	}
+
 	public int getStatus() {
 		return status;
 	}
+
 	public void setStatus(int status) {
 		this.status = status;
 	}
+
 	public Date getFecha_creado() {
 		return fecha_creado;
 	}
+
 	public void setFecha_creado(Date fecha_creado) {
 		this.fecha_creado = fecha_creado;
 	}
+
 	public Date getFecha_modificado() {
 		return fecha_modificado;
 	}
+
 	public void setFecha_modificado(Date fecha_modificado) {
 		this.fecha_modificado = fecha_modificado;
 	}
@@ -88,5 +100,4 @@ public class Visita {
 				+ ", status=" + status + ", fecha_creado=" + fecha_creado
 				+ ", fecha_modificado=" + fecha_modificado + "]";
 	}
-
 }
