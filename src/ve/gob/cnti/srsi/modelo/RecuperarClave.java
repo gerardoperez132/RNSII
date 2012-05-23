bbe73925a50ca4dce41bf2dc676b4d6749294810
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-05-23 09:35:20 -0430

    Agregando un comentario para visualizar la clase de Usuarios y cambiando el formato e indentación del código.

--- src/ve/gob/cnti/srsi/modelo/RecuperarClave.java
+++ src/ve/gob/cnti/srsi/modelo/RecuperarClave.java
@@ -24,8 +24,10 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
- * Esta clase sirve para registrar y auditar las peticiones de los 
- * usuarios para recuperar sus contraseñas olvidadas 
+ * Esta clase sirve para registrar y auditar las peticiones de los usuarios para
+ * recuperar sus contraseñas olvidadas
+ * 
+ * @see Usuario
  * 
  * @author Joaquín Pereira
  * 
@@ -33,7 +35,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "recuperar_clave")
 public class RecuperarClave {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
@@ -43,55 +45,72 @@ public class RecuperarClave {
 	private int status;
 	private Date fecha_creado;
 	private Date fecha_modificado;
-	
+
+	public RecuperarClave() {
+	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public long getId_recuperar_clave() {
 		return id_recuperar_clave;
 	}
+
 	public void setId_recuperar_clave(long id_recuperar_clave) {
 		this.id_recuperar_clave = id_recuperar_clave;
 	}
+
 	public long getId_usuario() {
 		return id_usuario;
 	}
+
 	public void setId_usuario(long id_usuario) {
 		this.id_usuario = id_usuario;
 	}
+
 	public String getUrl() {
 		return url;
 	}
+
 	public void setUrl(String url) {
 		this.url = url;
 	}
+
 	public int getStatus() {
 		return status;
 	}
+
 	public void setStatus(int status) {
 		this.status = status;
 	}
+
 	public Date getFecha_creado() {
 		return fecha_creado;
 	}
+
 	public void setFecha_creado(Date fecha_creado) {
 		this.fecha_creado = fecha_creado;
 	}
+
 	public Date getFecha_modificado() {
 		return fecha_modificado;
 	}
+
 	public void setFecha_modificado(Date fecha_modificado) {
 		this.fecha_modificado = fecha_modificado;
 	}
+
 	@Override
 	public String toString() {
 		return "RecuperarClave [id=" + id + ", id_recuperar_clave="
 				+ id_recuperar_clave + ", id_usuario=" + id_usuario + ", url="
 				+ url + ", status=" + status + ", fecha_creado=" + fecha_creado
 				+ ", fecha_modificado=" + fecha_modificado + "]";
-	}	
-	
+	}
+
 }
