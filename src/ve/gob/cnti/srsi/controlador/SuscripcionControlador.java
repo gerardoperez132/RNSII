c5bb1bc7780068aef88833d742035e25f4ed6e0c
Joaquín Pereira <pereira.joaquin@gmail.com>
2012-05-31 15:26:33 -0430

    Creando modelo y controlador de suscripciones

---
+++ src/ve/gob/cnti/srsi/controlador/SuscripcionControlador.java
@@ -0,0 +1,69 @@
+package ve.gob.cnti.srsi.controlador;
+
+import ve.gob.cnti.srsi.dao.Constants;
+import ve.gob.cnti.srsi.dao.DAO;
+import ve.gob.cnti.srsi.dao.Constants.Modelos;
+import ve.gob.cnti.srsi.dao.Constants.Order;
+import ve.gob.cnti.srsi.modelo.Ente;
+import ve.gob.cnti.srsi.modelo.ServicioInformacion;
+
+@SuppressWarnings("serial")
+public class SuscripcionControlador extends DAO implements Constants, Order,
+Modelos{
+	
+	private ServicioInformacion servicio = new ServicioInformacion();	
+	private Ente ente = new Ente();
+	
+	private long id_servicio;
+	private boolean suscripcion_form;
+	
+	public String prepaparaSuscripcion(){		
+		if (!verificarLong(id_servicio))
+			return INPUT;
+		servicio = (ServicioInformacion) read(servicio, id_servicio);
+		ente = (Ente) read(ente, servicio.getId_ente());
+		suscripcion_form = true;
+		return SUCCESS;
+	}
+	
+	public boolean verificarLong(long n) {
+		try {
+			return n != 0;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public ServicioInformacion getServicio() {
+		return servicio;
+	}
+
+	public void setServicio(ServicioInformacion servicio) {
+		this.servicio = servicio;
+	}
+
+	public Ente getEnte() {
+		return ente;
+	}
+
+	public void setEnte(Ente ente) {
+		this.ente = ente;
+	}
+
+	public long getId_servicio() {
+		return id_servicio;
+	}
+
+	public void setId_servicio(long id_servicio) {
+		this.id_servicio = id_servicio;
+	}
+
+	public boolean isSuscripcion_form() {
+		return suscripcion_form;
+	}
+
+	public void setSuscripcion_form(boolean suscripcion_form) {
+		this.suscripcion_form = suscripcion_form;
+	}	
+
+}
---
+++ src/ve/gob/cnti/srsi/modelo/SolicitudSuscripcion.java
@@ -0,0 +1,174 @@
+package ve.gob.cnti.srsi.modelo;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Richard Ricciardelli
+ * 
+ */
+@Entity
+@Table(name = "solicitudes_suscripciones")
+public class SolicitudSuscripcion {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private long id_solicitud_suscripcion;
+	private long id_ente_solicitante;
+	private long id_ente_proveedor;
+	private long id_servicio_informacion;
+	private boolean leido;
+	private int sentencia;
+	private int status;
+	private Date fecha_creado;
+	private Date fecha_modificado;
+	private String solicitante;
+	private String cargo;
+	private String telefono;
+	private String correo;
+	private String motivo;
+			
+	public SolicitudSuscripcion() {	
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getId_solicitud_suscripcion() {
+		return id_solicitud_suscripcion;
+	}
+
+	public void setId_solicitud_suscripcion(long id_solicitud_suscripcion) {
+		this.id_solicitud_suscripcion = id_solicitud_suscripcion;
+	}
+
+	public long getId_ente_solicitante() {
+		return id_ente_solicitante;
+	}
+
+	public void setId_ente_solicitante(long id_ente_solicitante) {
+		this.id_ente_solicitante = id_ente_solicitante;
+	}
+
+	public long getId_ente_proveedor() {
+		return id_ente_proveedor;
+	}
+
+	public void setId_ente_proveedor(long id_ente_proveedor) {
+		this.id_ente_proveedor = id_ente_proveedor;
+	}
+
+	public long getId_servicio_informacion() {
+		return id_servicio_informacion;
+	}
+
+	public void setId_servicio_informacion(long id_servicio_informacion) {
+		this.id_servicio_informacion = id_servicio_informacion;
+	}
+
+	public boolean isLeido() {
+		return leido;
+	}
+
+	public void setLeido(boolean leido) {
+		this.leido = leido;
+	}
+
+	public int getSentencia() {
+		return sentencia;
+	}
+
+	public void setSentencia(int sentencia) {
+		this.sentencia = sentencia;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Date getFecha_creado() {
+		return fecha_creado;
+	}
+
+	public void setFecha_creado(Date fecha_creado) {
+		this.fecha_creado = fecha_creado;
+	}
+
+	public Date getFecha_modificado() {
+		return fecha_modificado;
+	}
+
+	public void setFecha_modificado(Date fecha_modificado) {
+		this.fecha_modificado = fecha_modificado;
+	}
+
+	public String getSolicitante() {
+		return solicitante;
+	}
+
+	public void setSolicitante(String solicitante) {
+		this.solicitante = solicitante;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getMotivo() {
+		return motivo;
+	}
+
+	public void setMotivo(String motivo) {
+		this.motivo = motivo;
+	}
+
+	@Override
+	public String toString() {
+		return "SolicitudSuscripcion [id=" + id + ", id_solicitud_suscripcion="
+				+ id_solicitud_suscripcion + ", id_ente_solicitante="
+				+ id_ente_solicitante + ", id_ente_proveedor="
+				+ id_ente_proveedor + ", id_servicio_informacion="
+				+ id_servicio_informacion + ", leido=" + leido + ", sentencia="
+				+ sentencia + ", status=" + status + ", fecha_creado="
+				+ fecha_creado + ", fecha_modificado=" + fecha_modificado
+				+ ", solicitante=" + solicitante + ", cargo=" + cargo
+				+ ", telefono=" + telefono + ", correo=" + correo + ", motivo="
+				+ motivo + "]";
+	}
+		
+}
