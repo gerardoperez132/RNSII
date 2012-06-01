32ccdc7f58ad79e77a9823aee1ef2f28cb718f51
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-06-01 11:55:38 -0430

    Comentando una línea de código en esta función que prevenía el envío del formulario.

--- web/pages/res/js/suscripcion.js
+++ web/pages/res/js/suscripcion.js
@@ -1,11 +1,14 @@
-$(document).ready(function(){
-	/*******************************************************
-	 * 
-	 * Validación de formulario de Modificar clave
-	 * 
-	 ******************************************************/
-	$("#enviar_solicitud").click(function (e){	
-		e.preventDefault();
-		 $(document.createElement('span')).attr('class','error_pass').html('Acerda de funciona');
-	});
-});
+$(document).ready(
+		function() {
+			/*******************************************************************
+			 * 
+			 * Validación de formulario de Modificar clave
+			 * 
+			 ******************************************************************/
+			$("#enviar_solicitud").click(
+					function(e) {
+						/* e.preventDefault(); */
+						$(document.createElement('span')).attr('class',
+								'error_pass').html('Acerda de funciona');
+					});
+		});
