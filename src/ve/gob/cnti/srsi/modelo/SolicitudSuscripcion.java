c1e4b4111dfbf68bde7a1f80663aff69068848dd
Joaquín Pereira <pereira.joaquin@gmail.com>
2012-06-11 09:55:02 -0430

    Agregando motivo del proveedor y del solicitante

--- src/ve/gob/cnti/srsi/modelo/SolicitudSuscripcion.java
+++ src/ve/gob/cnti/srsi/modelo/SolicitudSuscripcion.java
@@ -38,139 +38,112 @@ public class SolicitudSuscripcion {
 	private String cargo;
 	private String telefono;
 	private String correo;
-	private String motivo;
-
-	public SolicitudSuscripcion() {
-	}
-
+	private String motivo_solicitante;
+	private String motivo_proveedor;
+	
 	public long getId() {
 		return id;
 	}
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
 	public long getId_solicitud_suscripcion() {
 		return id_solicitud_suscripcion;
 	}
-
 	public void setId_solicitud_suscripcion(long id_solicitud_suscripcion) {
 		this.id_solicitud_suscripcion = id_solicitud_suscripcion;
 	}
-
 	public long getId_ente_solicitante() {
 		return id_ente_solicitante;
 	}
-
 	public void setId_ente_solicitante(long id_ente_solicitante) {
 		this.id_ente_solicitante = id_ente_solicitante;
 	}
-
 	public long getId_ente_proveedor() {
 		return id_ente_proveedor;
 	}
-
 	public void setId_ente_proveedor(long id_ente_proveedor) {
 		this.id_ente_proveedor = id_ente_proveedor;
 	}
-
 	public long getId_servicio_informacion() {
 		return id_servicio_informacion;
 	}
-
 	public void setId_servicio_informacion(long id_servicio_informacion) {
 		this.id_servicio_informacion = id_servicio_informacion;
 	}
-
-	public boolean isLeido() {
-		return leido;
-	}
-
-	public void setLeido(boolean leido) {
-		this.leido = leido;
-	}
-
-	public int getSentencia() {
-		return sentencia;
-	}
-
-	public void setSentencia(int sentencia) {
-		this.sentencia = sentencia;
-	}
-
-	public int getStatus() {
-		return status;
-	}
-
-	public void setStatus(int status) {
-		this.status = status;
-	}
-
-	public Date getFecha_creado() {
-		return fecha_creado;
-	}
-
-	public void setFecha_creado(Date fecha_creado) {
-		this.fecha_creado = fecha_creado;
-	}
-
-	public Date getFecha_modificado() {
-		return fecha_modificado;
-	}
-
-	public void setFecha_modificado(Date fecha_modificado) {
-		this.fecha_modificado = fecha_modificado;
-	}
-
-	public String getSolicitante() {
-		return solicitante;
-	}
-
-	public void setSolicitante(String solicitante) {
-		this.solicitante = solicitante;
-	}
-
-	public String getCargo() {
-		return cargo;
-	}
-
-	public void setCargo(String cargo) {
-		this.cargo = cargo;
-	}
-
-	public String getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-
-	public String getCorreo() {
-		return correo;
-	}
-
-	public void setCorreo(String correo) {
-		this.correo = correo;
-	}
-
-	public String getMotivo() {
-		return motivo;
-	}
-
-	public void setMotivo(String motivo) {
-		this.motivo = motivo;
-	}
-
 	public long getId_usuario() {
 		return id_usuario;
 	}
-
 	public void setId_usuario(long id_usuario) {
 		this.id_usuario = id_usuario;
 	}
-
+	public boolean isLeido() {
+		return leido;
+	}
+	public void setLeido(boolean leido) {
+		this.leido = leido;
+	}
+	public int getSentencia() {
+		return sentencia;
+	}
+	public void setSentencia(int sentencia) {
+		this.sentencia = sentencia;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public Date getFecha_creado() {
+		return fecha_creado;
+	}
+	public void setFecha_creado(Date fecha_creado) {
+		this.fecha_creado = fecha_creado;
+	}
+	public Date getFecha_modificado() {
+		return fecha_modificado;
+	}
+	public void setFecha_modificado(Date fecha_modificado) {
+		this.fecha_modificado = fecha_modificado;
+	}
+	public String getSolicitante() {
+		return solicitante;
+	}
+	public void setSolicitante(String solicitante) {
+		this.solicitante = solicitante;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getMotivo_solicitante() {
+		return motivo_solicitante;
+	}
+	public void setMotivo_solicitante(String motivo_solicitante) {
+		this.motivo_solicitante = motivo_solicitante;
+	}
+	public String getMotivo_proveedor() {
+		return motivo_proveedor;
+	}
+	public void setMotivo_proveedor(String motivo_proveedor) {
+		this.motivo_proveedor = motivo_proveedor;
+	}
+	
 	@Override
 	public String toString() {
 		return "SolicitudSuscripcion [id=" + id + ", id_solicitud_suscripcion="
@@ -182,6 +155,8 @@ public class SolicitudSuscripcion {
 				+ status + ", fecha_creado=" + fecha_creado
 				+ ", fecha_modificado=" + fecha_modificado + ", solicitante="
 				+ solicitante + ", cargo=" + cargo + ", telefono=" + telefono
-				+ ", correo=" + correo + ", motivo=" + motivo + "]";
-	}
+				+ ", correo=" + correo + ", motivo_solicitante="
+				+ motivo_solicitante + ", motivo_proveedor=" + motivo_proveedor
+				+ "]";
+	}	
 }
