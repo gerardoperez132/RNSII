10c3e0b6b678853e3acb6e48fe0c752a975d30e3
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-06-29 15:42:50 -0430

    Cambiando los comentarios por correcciones de redacción y agregando los respectivos autores de la implementación y conceptualización de los métodos expuestos.

--- src/ve/gob/cnti/srsi/dao/CRUD.java
+++ src/ve/gob/cnti/srsi/dao/CRUD.java
@@ -32,6 +32,7 @@ import ve.gob.cnti.srsi.util.Solicitud_Suscripcion;
  * Definición de métodos para las operaciones en la base de datos.
  * 
  * @author Richard Ricciardelli
+ * @author Joaquín Pereira
  * @see DAO
  * 
  */
@@ -40,6 +41,7 @@ public interface CRUD {
 	/**
 	 * Permite guardar un registro en la base de datos de cualquier modelo.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @param model
 	 *            Es la clase, modelo o tabla en el cual se realizará el
 	 *            registro.
@@ -50,6 +52,8 @@ public interface CRUD {
 	 * Permite guardar un registro en la base de datos cuando el modelo es de
 	 * una relación de unión M:N.
 	 * 
+	 * @author Richard Ricciardelli
+	 * @author Joaquín Pereira
 	 * @param model
 	 *            Es la clase, modelo o tabla en el cual se realizará el
 	 *            registro.
@@ -60,6 +64,8 @@ public interface CRUD {
 	 * Permite eliminar un registro en la base de datos cuando el modelo es de
 	 * una relación de unión M:N.
 	 * 
+	 * @author Richard Ricciardelli
+	 * @author Joaquín Pereira
 	 * @param model
 	 *            Es la clase, modelo o tabla en el cual se realizará la
 	 *            eliminación.
@@ -77,6 +83,7 @@ public interface CRUD {
 	/**
 	 * Permite obtener todos los registros activos del modelo dado.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @param model
 	 *            Es la clase, modelo o tabla en la cual se realizará la
 	 *            consulta.
@@ -105,6 +112,8 @@ public interface CRUD {
 	 * Permite obtener el registro activo especificado por el identificador del
 	 * modelo dado.
 	 * 
+	 * @author Richard Ricciardelli
+	 * @author Joaquín Pereira
 	 * @param model
 	 *            Es la clase, modelo o tabla en la cual se realizará la
 	 *            consulta.
@@ -119,6 +128,7 @@ public interface CRUD {
 	 * arreglo. Funciona para relaciones 1:M donde el segundo modelo sirve de
 	 * llave foránea dentro de la primer tabla.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @param models
 	 *            Son las clases, modelos o tablas en las cuales se realizará la
 	 *            consulta. Siendo la primera de éstas el tipo de objeto
@@ -138,6 +148,7 @@ public interface CRUD {
 	 * datos de acuerdo a un criterio de relación 1:N donde el segundo modelo
 	 * sirve de llave foránea.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @param models
 	 *            Son las clases, modelos o tablas en las cuales se realizará la
 	 *            consulta. Siendo la primera de éstas el tipo de objeto
@@ -155,6 +166,7 @@ public interface CRUD {
 	 * Permite obtener una lista de objetos del modelo dado ordenado por nombre
 	 * de forma ascendente o descendente dependiendo del caso.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @param model
 	 *            Es la clase, modelo o tabla en la cual se realizará la
 	 *            consulta.
@@ -167,6 +179,8 @@ public interface CRUD {
 	/**
 	 * Permite obtener el objeto correo de un usuario con el string dado.
 	 * 
+	 * @author Richard Ricciardelli
+	 * @author Joaquín Pereira
 	 * @param email
 	 *            El string del correo que se quiere buscar.
 	 * @return {@code correo} asociado a un usuario.
@@ -176,6 +190,8 @@ public interface CRUD {
 	/**
 	 * Permite obtener el objeto correo de un identificador de modelo dado.
 	 * 
+	 * @author Richard Ricciardelli
+	 * @author Joaquín Pereira
 	 * @param model
 	 *            Es la clase, modelo o tabla en la cual se realizará la
 	 *            consulta.
@@ -188,6 +204,7 @@ public interface CRUD {
 	/**
 	 * Permite modificar el registro especificado por id del modelo dado.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @param model
 	 *            Es la clase, modelo o tabla en la cual se realizará la
 	 *            consulta.
@@ -200,6 +217,8 @@ public interface CRUD {
 	 * Permite modificar el registro especificado por id del modelo resultante
 	 * de una relación M:N.
 	 * 
+	 * @author Richard Ricciardelli
+	 * @author Joaquín Pereira
 	 * @param unionModel
 	 *            Es el modelo resultante de una relación M:N.
 	 * @param modelParent
@@ -221,6 +240,7 @@ public interface CRUD {
 	/**
 	 * Permite eliminar el registro especificado por id del modelo dado.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @param model
 	 *            Es la clase, modelo o tabla en la cual se realizará la
 	 *            consulta.
@@ -233,6 +253,7 @@ public interface CRUD {
 	 * Permite obtener el id lógico correspondiente al modelo dado para poder
 	 * crear un nuevo registro.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @param model
 	 *            Es la clase, modelo o tabla en la cual se realizará la
 	 *            consulta.
@@ -244,6 +265,7 @@ public interface CRUD {
 	 * Permite obtener el campo que será utilizado para obtener el id propio de
 	 * cada modelo.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @param model
 	 *            Es la clase, modelo o tabla en la cual se realizará la
 	 *            consulta.
@@ -255,6 +277,7 @@ public interface CRUD {
 	 * Permite obtener el sufijo para ser agregado al campo que será utilizado
 	 * para la consulta del id de cada modelo.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @param model
 	 *            Es la clase, modelo o tabla en la cual se realizará la
 	 *            consulta.
@@ -275,6 +298,7 @@ public interface CRUD {
 	/**
 	 * Permite obtener los registros "hijo" de un modelo determinado.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @param model
 	 *            Es la clase, modelo o tabla en la cual se realizará la
 	 *            consulta.
@@ -285,6 +309,7 @@ public interface CRUD {
 	/**
 	 * Permite obtener la lista de los datos simples cargados en base de datos.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @return Lista de datos simples.
 	 */
 	public ArrayList<?> getSimple();
@@ -293,13 +318,15 @@ public interface CRUD {
 	 * Permite obtener la lista de los datos compuestos cargados en base de
 	 * datos.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @return Lista de datos compuestos.
 	 */
 	public ArrayList<?> getComplex();
 
 	/**
-	 * Permite obtener el telefono unsa clase dada
+	 * Permite obtener el teléfono de una clase
 	 * 
+	 * @author Joaquín Pereira
 	 * @param model
 	 *            Es la clase, modelo o tabla en la cual se realizará la
 	 *            consulta.
@@ -313,6 +340,8 @@ public interface CRUD {
 	 * Permite actualizar una misma tupla sobre los datos faltantes. No genera
 	 * trazas de auditoría.
 	 * 
+	 * @author Richard Ricciardelli
+	 * @author Joaquín Pereira
 	 * @param model
 	 *            Es la clase, modelo o tabla en la cual se realizará la
 	 *            consulta.
@@ -335,12 +364,14 @@ public interface CRUD {
 	 * Permite saber si existe un registro activo mediante la llave foránea con
 	 * las relaciones de las dos entidades, pasadas como argumentos.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @param models
 	 *            Son las dos entidades relacionadas, primero la entidad padre y
 	 *            luego la entidad hija
 	 * @param id
 	 *            Clave foránea de la entidad hija
-	 * @return Valor booleano en caso de que exista o no, un registro activo
+	 * @return <code>true</code> en caso de que exista un registro válido,
+	 *         <code>false</code> en caso contrario
 	 */
 	public boolean read(Object[] models, long id);
 
@@ -348,6 +379,7 @@ public interface CRUD {
 	 * Permite saber si existe un registro activo mediante la llave foránea con
 	 * las relaciones de las dos entidades, pasadas como argumentos.
 	 * 
+	 * @author Joaquín Pereira
 	 * @param models
 	 *            Son las dos entidades relacionadas, primero la entidad padre y
 	 *            luego la entidad hija
@@ -362,6 +394,7 @@ public interface CRUD {
 	 * Permite saber si existe un registro activo mediante la llave foránea con
 	 * las relaciones de las dos entidades, pasadas como argumentos.
 	 * 
+	 * @author Joaquín Pereira
 	 * @param models
 	 *            Son las dos entidades relacionadas, primero la entidad padre y
 	 *            luego la entidad hija
@@ -376,6 +409,7 @@ public interface CRUD {
 	 * Permite saber el número de servicios de información publicados en el
 	 * sector solicitado.
 	 * 
+	 * @author Joaquín Pereira
 	 * @param id
 	 *            id del sector a consultar
 	 * @return retorna el número de servicios publicados del sector
@@ -387,6 +421,7 @@ public interface CRUD {
 	 * Permite buscar servicios en el que el nombre de estos, se asemejen a la
 	 * cadena pasada
 	 * 
+	 * @author Joaquín Pereira
 	 * @param cadena
 	 *            Es la cadena a consultar
 	 * 
@@ -398,6 +433,8 @@ public interface CRUD {
 	/**
 	 * Permite guardar una visita.
 	 * 
+	 * @author Richard Ricciardelli
+	 * @author Joaquín Pereira
 	 * @param visita
 	 *            Visita a guardar.
 	 */
@@ -407,6 +444,7 @@ public interface CRUD {
 	 * Permite obtener el número de visitas de un servicio de información de
 	 * acuerdo a su identificador.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @param id
 	 *            Identificador del servicio de información
 	 * @return Cantidad de visitas del servicio de información correspondiente
@@ -418,6 +456,7 @@ public interface CRUD {
 	 * Permite verificar el acceso de un cliente en un límite de tiempo
 	 * determinado para poder contarse como una visita única.
 	 * 
+	 * @author Richard Ricciardelli
 	 * @param ip
 	 *            Dirección IP del cliente que está realizando la visita
 	 * @return {@code true} si es una visita nueva fuera del lapso establecido,
@@ -428,6 +467,7 @@ public interface CRUD {
 	/**
 	 * Permite Consultar los 5 servicios de información más visitados.
 	 * 
+	 * @author Joaquín Pereira
 	 * @return Retorna una lista con los servicios de información más visitados.
 	 */
 	public List<ListaSImasVisitados> SImasVisitados();
@@ -436,6 +476,7 @@ public interface CRUD {
 	 * Permite consultar una cantidad n de sectores con más servicios de
 	 * información implementados y publicados.
 	 * 
+	 * @author Joaquín Pereira
 	 * @param n
 	 *            Número para limitar la consulta, un valor menor o igual a cero
 	 *            se considera como una consulta sin limitante.
@@ -445,9 +486,10 @@ public interface CRUD {
 	public List<SectoresMasPublicados> sectoresMasPublicados(int n);
 
 	/**
-	 * Permite Consultar los servicios de información implemantados y
-	 * públicados.
+	 * Permite consultar los servicios de información implementados y
+	 * publicados.
 	 * 
+	 * @author Joaquín Pereira
 	 * @param n
 	 *            Numero para limitar la consulta, un valor menor o igual a cero
 	 *            se considera como una consulta sin limitante.
@@ -461,6 +503,7 @@ public interface CRUD {
 	 * Retorna una lista de los servicios de información implementados, activos
 	 * y publicados en el sector dado.
 	 * 
+	 * @author Joaquín Pereira
 	 * @param id_sector
 	 *            Identificador del sector al cual pertenecen los servicios de
 	 *            información.
@@ -470,9 +513,10 @@ public interface CRUD {
 			long id_sector, byte orderBy);
 
 	/**
-	 * Permite buscar servicios en el que el nombre de estos, se asemejen a la
+	 * Permite buscar servicios en el que el nombre de estos se asemejen a la
 	 * cadena pasada
 	 * 
+	 * @author Joaquín Pereira
 	 * @param cadena
 	 *            Es la cadena a consultar id_ente Sirve para discriminar los
 	 *            servicio de información del propio ente
@@ -483,9 +527,10 @@ public interface CRUD {
 			byte orderBy, long id_ente);
 
 	/**
-	 * Permite saber el número solicitudes de suscripción no leidos que posee el
+	 * Permite saber el número solicitudes de suscripción no leídos que posee el
 	 * ente
 	 * 
+	 * @author Joaquín Pereira
 	 * @param id
 	 *            id del ente a consultar
 	 * @return retorna el número solicitudes de suscripción no leidas
@@ -497,6 +542,7 @@ public interface CRUD {
 	 * Permite saber el número solicitudes de suscripción pendientes que posee
 	 * el ente
 	 * 
+	 * @author Joaquín Pereira
 	 * @param id
 	 *            id del ente a consultar
 	 * @return retorna el número solicitudes de suscripción pendientes
@@ -508,6 +554,7 @@ public interface CRUD {
 	 * Retorna una lista de las solicitudes de suscrición a los servicios de
 	 * información publicados por el ente
 	 * 
+	 * @author Joaquín Pereira
 	 * @param id_ente
 	 *            Identificador del ente al cual pertenecen las solicitudes de
 	 *            suscripción a los servicios de información publicados.
@@ -518,9 +565,10 @@ public interface CRUD {
 			long id_ente, byte orderBy);
 
 	/**
-	 * Permite saber el número solicitudes de suscripción Aceptadas-Rechazadas
-	 * por leer
+	 * Permite saber el número de solicitudes de suscripción
+	 * Aceptadas-Rechazadas por leer.
 	 * 
+	 * @author Joaquín Pereira
 	 * @param id
 	 *            id del ente a consultar
 	 * @return retorna el número solicitudes de suscripción Aceptadas-Rechazadas
@@ -532,6 +580,7 @@ public interface CRUD {
 	/**
 	 * Retorna una lista de las solicitudes de suscrición Aceptadas - Rechazadas
 	 * 
+	 * @author Joaquín Pereira
 	 * @param id_ente
 	 *            Identificador del ente al cual pertenecen las solicitudes de
 	 *            suscripción aceptadas - rechazadas a buscar.
@@ -544,6 +593,7 @@ public interface CRUD {
 	 * Permite obtener las solicitudes de suscripción al servicio del proveedor
 	 * dado por parte de un cliente.
 	 * 
+	 * @author Joaquín Pereira
 	 * @param service
 	 *            Servicio de información solicitado
 	 * @param provider
@@ -560,6 +610,7 @@ public interface CRUD {
 	 * Permite verificar si una solicitud de suscripción existe actualmente o
 	 * no.
 	 * 
+	 * @author Joaquín Pereira
 	 * @param service
 	 *            Servicio de información solicitado
 	 * @param providerProveedor
