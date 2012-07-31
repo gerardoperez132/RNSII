27b938d0ea2370da58400e3d7b3310c4ede479d4
Joaquín Pereira <pereira.joaquin@gmail.com>
2012-07-31 10:31:11 -0430

    Adecuando estos js para la  visualización de GE

--- web/pages/res/js/registro/formulario_entrada.js
+++ web/pages/res/js/registro/formulario_entrada.js
@@ -1,6 +1,32 @@
 var isLongitud;
 var isFormato;
 
+/*
+ * funciones para el correcto funcionamiento de links de la vista de gobierno en linea
+ */
+function changeValues(page, campoId1, valor1, campoId2, valor2, form){
+    var campo1 = document.getElementById(campoId1);
+    campo1.value = valor1;
+    var campo2 = document.getElementById(campoId2);
+    campo2.value = valor2;                      
+    submitForm(page, form);	   
+}
+function changeValue(page, campoId, valor, form){
+	var campo = document.getElementById(campoId);
+	campo.value = valor;			
+	submitForm(page, form);
+}
+function submitForm(page, form){
+    var form = document.getElementById(form);
+    form.action= "http://gobiernoenlinea.gob.ve/home/" + page;	    
+    form.submit();
+}	
+function actualizarClima(cadena){
+	var T = cadena.split(",");
+    $('#t_max').html(''+T[0]);
+    $('#t_min').html(''+T[1]);
+}
+
 $.validator.addMethod('regexTitle', function (value) {
 	var soloTexto = new RegExp("^[a-zA-Z áéíóúAÉÍÓÚÑñ]+$");		    	
 	return soloTexto.test(value);
@@ -170,5 +196,23 @@ $(document).ready(function(){
 		
 	}
 	
+	/*
+	 * funciones para el correcto funcionamiento de links de la vista de gobierno en linea
+	 */	
+	$(".masPie").hide();
+    
+    $(".cerrarPie").toggle(
+     function () {
+        $(".masPie").slideDown();
+        $(".cerrarPie").text("-");
+ 		return false;
+     },
+     function () {
+        $(".masPie").slideUp();
+        $(".cerrarPie").text("+");
+ 		return false;                    
+     }
+   );
 	
 });
+
