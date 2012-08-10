e805fe7ba3812bbf56520dd8cac61a53f253db74
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-08-10 10:34:00 -0430

    Agregando una longitud para la columna de descripción en el modelo de servicios de información.

--- src/ve/gob/cnti/srsi/modelo/ServicioInformacion.java
+++ src/ve/gob/cnti/srsi/modelo/ServicioInformacion.java
@@ -17,6 +17,7 @@ package ve.gob.cnti.srsi.modelo;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -46,6 +47,7 @@ public class ServicioInformacion {
 	private long id_ente;
 	private long id_usuario;
 	private String nombre;
+	@Column(length = 765)
 	private String descripcion;
 	private String version;
 	private String responsable;
