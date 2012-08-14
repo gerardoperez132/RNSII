da24f37261ca489dbc386b0ad9a966568a119020
Joaquín Pereira <pereira.joaquin@gmail.com>
2012-08-14 10:49:06 -0430

    Agregando la internacionalización a este archivo js

--- web/pages/res/js/registro/funcionalidades.js
+++ web/pages/res/js/registro/funcionalidades.js
@@ -1,4 +1,26 @@
 /*
+ * Variable con las claves de intercionalización del archivo json. 
+ */
+var data;
+
+$(document).ready(function() {	
+	
+	/*
+	 * Obteniendo los valores de intercionalización del archivo JSON
+	 */
+	$.ajax({
+		url: "getJSONResult.action",		
+		type: "GET",
+		dataType: "json",
+		async:false,		
+		success: function(source){	
+			data = source;			
+		}	
+	});
+	
+});
+
+/* 
  * Función que muestra unos cuadro de dialogos hecho con el plugin sexy alert
  * que sirven para eliminar un servicio de información.
  * 
@@ -9,8 +31,8 @@
 function eliminar_Fun(i,nombre){
 	var action = 'id_'+i;	
 	var formulario = document.getElementById(action);	
-	Sexy.confirm('Está a punto de eliminar la Funcionalidad: <br>'+
-			'<br><h3>'+nombre+'</h3> <br>¿Desea Continuar?', {
+	Sexy.confirm(data['mensajes']['eliminar_funcionalidad']+'<br>'+
+			'<br><h3>'+nombre+'</h3> <br>'+data['mensajes']['desea_continuar'], {
 	  onComplete:
 	    function(returnvalue) {
 	      if (returnvalue) {
@@ -22,22 +44,29 @@ function eliminar_Fun(i,nombre){
     		  });
 	    		  
 	      } else {
-	    	  Sexy.alert('Acción Cancelada');
+	    	  Sexy.alert(data['mensajes']['accion_cancelada']);
 	      }
 	    }
 	  });			
 };
 
+/* 
+ * Función que muestra unos cuadro de dialogos hecho con el plugin sexy alert
+ * que sirven para abandonar la carga de datos para un servicio de información.
+ * 
+ * parametros:
+ * 		nombre = nombre del servicio de información 
+ */
 function salir(nombre){	
 	var formulario = document.getElementById('salir');	
-	Sexy.confirm('Está abandonar la carga de datos para este servicio de información: <br>'+
-			'<br><h3>'+nombre+'</h3> <br>¿Desea Continuar?', {
+	Sexy.confirm(data['mensajes']['abandonar_carga_datos_si']+' <br>'+
+			'<br><h3>'+nombre+'</h3> <br>'+data['mensajes']['desea_continuar'], {
 	  onComplete:
 	    function(returnvalue) {
 	      if (returnvalue) {	    	  
 	    	  formulario.submit();
 	      } else {
-	    	  Sexy.alert('Acción Cancelada');
+	    	  Sexy.alert(data['mensajes']['accion_cancelada']);
 	      }
 	    }
 	  });			
