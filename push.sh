8db3cfaddd48ffae2eb4983424d48d7aa3548913
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-08-20 14:56:35 -0430

    Intentando agregar negrillas.

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 REMOTES=$(git remote)
 for i in $REMOTES;
 do
-  echo $(tput bold)"\nUploading changes to $i please wait..."
+  echo ${$(tput bold)}"\nUploading changes to $i please wait..."
   git push $i permisos
 done
