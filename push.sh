a1106c3a66d83eb06cd0c00acecf7505dd7416eb
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-08-24 11:17:57 -0430

    Borrando este script...

--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-REMOTES=$(git remote)
-for i in $REMOTES;
-do
-  printf '\n'
-  echo $(tput bold)"Branch => $1..."$(tput sgr0)
-  echo $(tput bold)"Uploading changes to $i please wait..."$(tput sgr0)
-  git push $i $1
-done
