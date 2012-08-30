6957f931c0376843d6d72026fda83523d97a65db
Joaquín Pereira <pereira.joaquin@gmail.com>
2012-08-30 16:49:25 -0430

    Cambiando el menu del usuario autenticado

--- web/pages/layout/sidebar.jsp
+++ web/pages/layout/sidebar.jsp
@@ -1,37 +1,44 @@
 <%@taglib uri="/struts-tags" prefix="s"%>
-<div class="sidebar">
-	
-	<div class="secciones" style="clear: both;">		
-		<ul class="ui-menu-content">
-			<li class="li-menu-content">				
-				<a href="<s:url action="home"/>" title="Inicio"><s:text name="home" /></a>				
-			</li>	
-			<li class="li-menu-content">				
-				<a><s:text name="servicio_informacion" /></a>				
-			</li>
-			<li class="li-menu-content">				
-				<a style="padding-left: 10px;" href="<s:url action="prepararRegistro"/>"><s:text name="registro" /></a>				
-			</li>
-			<!-- 
-			<li class="li-menu-content">				
-				<a style="padding-left: 10px;" href="#"><s:text name="suscripciones" /></a>		
-			</li>			
-			<li class="li-menu-content">				
-				<a style="padding-left: 25px;" href="<s:url action="prepararSolicitarSuscripcion"/>"><s:text name="solicitud3" /></a>			
-			</li>
-			<li class="li-menu-content">				
-				<a style="padding-left: 25px;" href="<s:url action="ListarSuscricionesPendientes"/>"><s:text name="peticiones_otros_entes" /></a>				
+<div class="sidebar">	
+	<div class="m_a">
+    <ul>
+        <li class="m_a_fondo">
+            <a class="m_a_titulo" href="<s:url action="home"/>">
+            	<s:text name="home" />            	
+            </a>
+        </li>
+        <li class="m_a_fondo">
+            <a class="m_a_titulo">
+            	<s:text name="servicio_informacion" />
+            	<span class="m_a_desplegar">+</span>
+            </a>            
+            <ul class="m_a_top" style="display: block;">
+                <li>
+                <a class="m_a_item"  href="<s:url action="prepararRegistro"/>">
+                	<s:text name="registro" />
+                </a>
+                </li>                
+            </ul>
+        </li>
+        <li class="m_a_fondo">
+            <a class="m_a_titulo" href="<s:url action="configuracionUsuario"/>">
+            	<s:text name="configuración" />            	
+            </a> 
+        </li>
+        <li class="m_a_fondo">
+            <a class="m_a_titulo" href="<s:url action="salir"/>">
+            	<s:text name="salir" />            	
+            </a> 
+        </li>
+    </ul>       
+    </div>
+    <!-- 
+							
+			<ahref="<s:url action="prepararSolicitarSuscripcion"/>"><s:text name="solicitud3" /></a>			
+			<a style="padding-left: 25px;" href="<s:url action="ListarSuscricionesPendientes"/>"><s:text name="peticiones_otros_entes" /></a>				
 			</li>			
 			<li class="li-menu-content">				
 				<a style="padding-left: 25px;" href="<s:url action="listarSolicitudesAceptadasRechazadas"/>"><s:text name="solicitudeRealizadas" /></a>				
 			</li>
 			 -->
-			<li class="li-menu-content">				
-				<a href="<s:url action="configuracionUsuario"/>"><s:text name="configuración" /></a>				
-			</li>
-			<li class="li-menu-content">				
-				<a href="<s:url action="salir"/>" title="Cerrar sesión"><s:text name="salir" /></a>				
-			</li>
-		</ul>		
-	</div>
 </div>
--- web/pages/res/js/funciones_ge.js
+++ web/pages/res/js/funciones_ge.js
@@ -1,40 +1,137 @@
 $(document).ready(function() {
-		$("#tree").treeTable();
-		
-		$(".masPie").hide();
-	    
-	    $(".cerrarPie").toggle(
-	         function () {
-	            $(".masPie").slideDown();
-	            $(".cerrarPie").text("-");
-	     		return false;
-	         },
-	         function () {
-	            $(".masPie").slideUp();
-	            $(".cerrarPie").text("+");
-	     		return false;                    
-	         }
-	       );	    
-	});
-	function changeValues(page, campoId1, valor1, campoId2, valor2, form){
-	    var campo1 = document.getElementById(campoId1);
-	    campo1.value = valor1;
-	    var campo2 = document.getElementById(campoId2);
-	    campo2.value = valor2;                      
-	    submitForm(page, form);	   
-	}
-	function changeValue(page, campoId, valor, form){
-		var campo = document.getElementById(campoId);
-		campo.value = valor;			
-		submitForm(page, form);
-	}
-	function submitForm(page, form){
-	    var form = document.getElementById(form);
-	    form.action= "http://gobiernoenlinea.gob.ve/home/" + page;	    
-	    form.submit();
-	}	
-	function actualizarClima(cadena){
-		var T = cadena.split(",");
-	    $('#t_max').html(''+T[0]);
-	    $('#t_min').html(''+T[1]);
-	}
+	$("#tree").treeTable();
+	
+	$(".masPie").hide();
+    
+    $(".cerrarPie").toggle(
+         function () {
+            $(".masPie").slideDown();
+            $(".cerrarPie").text("-");
+     		return false;
+         },
+         function () {
+            $(".masPie").slideUp();
+            $(".cerrarPie").text("+");
+     		return false;                    
+         }
+       );	
+    
+    $('.m_a').lksMenu();
+    
+    $(".m_a ul li ul").attr("style", "display: none");
+    $(".m_a ul li ul li ul").attr("style", "display: none");
+});
+
+function changeValues(page, campoId1, valor1, campoId2, valor2, form){
+    var campo1 = document.getElementById(campoId1);
+    campo1.value = valor1;
+    var campo2 = document.getElementById(campoId2);
+    campo2.value = valor2;                      
+    submitForm(page, form);	   
+}
+function changeValue(page, campoId, valor, form){
+	var campo = document.getElementById(campoId);
+	campo.value = valor;			
+	submitForm(page, form);
+}
+function submitForm(page, form){
+    var form = document.getElementById(form);
+    form.action= "http://gobiernoenlinea.gob.ve/home/" + page;	    
+    form.submit();
+}	
+function actualizarClima(cadena){
+	var T = cadena.split(",");
+    $('#t_max').html(''+T[0]);
+    $('#t_min').html(''+T[1]);
+}	
+
+(function($){
+    // fn es un shortcut al prototipo (prototypo) de la libreria jquery
+    // declarando un metodo dentro de esta librería la extendemos para ser usada
+    // con cualquier selector
+    $.fn.lksMenu=function(){
+        // para mantener la posibilidad de concatenar metodos es que retornamos la función en
+        // lugar de solo ejecutar algo y ya.
+        // en este caso usamos un each, porque el selector sobre el que aplicamos la funcion
+        // podria contener mas de un elemento , esto es , aplicaria $('.menu').menu() lo cual
+        // ejecutaria el codigo para todos los elementos que tengan la clase menu, como puede
+        // haber mas de uno es que ejecutamos el each para que corra sobre todos ellos
+        return this.each(function(){
+            // obtenemos el elemento que se esta analizando en esta vuelta del each
+            var menu= $(this);
+            // localizamos los links principales y le asignamos un evento click
+            menu.find('ul li > a').bind('click',function(event){
+                // identificamos el elemento sobre el que se hizo click
+                var currentlink=$(event.currentTarget);
+                // los ul que tengan la clase active seran los que estan abiertos
+                // si el link sobre que presionamos ya estaba abierto , es decir tenia
+                // la clase active, lo cerramos y nada mas
+                if (currentlink.parent().find('ul.active').size()==1)
+                {                	
+                    //cerramos el link porque apretamos sobre el mismo abierto
+                    currentlink.parent().find('ul.active').slideUp('medium',function(){
+                        // le quitamos la clase
+                        currentlink.parent().find('ul.active').removeClass('active');
+                    });
+                }
+                // si ningun link estaba apretado
+                else if (menu.find('ul li ul.active').size()==0)
+                {
+                    //no hace falta cerrar ninguno y solo abrimos el elemento al
+                    // cual se le hizo click
+                    show(currentlink);
+                }
+                else
+                {
+                    // si ya habia un item abierto , simplemente lo localizamos
+                    // con find, y lo cerramos con slideup,
+                    menu.find('ul li ul.active').slideUp('medium',function(){
+                        // una vez que cerramos el que estaba abierto
+                        // le quitamos la clase active
+                        menu.find('ul li ul').removeClass('active');
+                        // abrimos uno nuevo que corresponde al que se clickeo
+                        show(currentlink);
+                    });
+                }
+            });
+            
+            
+            menu.find('ul li > a').bind('click',function(event){
+                
+                var currentlink=$(event.currentTarget);
+                
+                if (currentlink.parent().find('span.active').size()==1)
+                {                    
+                    currentlink.parent().find('span.active').html("+");
+                    currentlink.parent().find('span.active').removeClass('active');                    
+                }
+                else if (menu.find('span.active').size()==0)
+                {                                        
+                    show2(currentlink);
+                }
+                else
+                {
+                        menu.find('span').html("+");
+                        menu.find('span').removeClass('active');
+                        show2(currentlink);
+                }
+            });
+            
+            
+
+            // esta funcion es de soporte
+            // todo lo que hace es abrir el elemento y asignarle la clase active
+            function show(currentlink){
+                currentlink.parent().find('ul').addClass('active');
+                currentlink.parent().find('ul').slideDown('medium');
+            }            
+            
+            function show2(currentlink){
+                currentlink.parent().find('span').addClass('active');  
+                currentlink.parent().find('span.active').html("-");              
+            }
+        });
+    }
+    // esto es lo que deciamos al principio , ejecutamos la funcion por eso los
+    // parentesis y le pasamos por parametro la libreria jQuery.
+})(jQuery);
