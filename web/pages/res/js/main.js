e90d7dba9de823558b5f2d6fa17009c55e362bca
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-11-08 09:52:14 -0430

    Formato.

--- web/pages/res/js/main.js
+++ web/pages/res/js/main.js
@@ -3,35 +3,36 @@
  */
 var data;
 
-$(document).ready(function(){	
-	
+$(document).ready(function() {
+
 	/*
 	 * Obteniendo los valores de intercionalización del archivo JSON
 	 */
 	$.ajax({
-		url: "getJSONResult.action",		
-		type: "GET",
-		dataType: "json",
-		async:false,		
-		success: function(source){	
-			data = source;			
-		}	
+		url : "getJSONResult.action",
+		type : "GET",
+		dataType : "json",
+		async : false,
+		success : function(source) {
+			data = source;
+		}
 	});
-	
+
 	/*
 	 * LLenando variable que lee el número de servicios incompletos
 	 */
 	var n = $('div.n').html();
-	
+
 	/*
-	 * Creación del número de capas necesarias para la visualización de los sexys tool tips
+	 * Creación del número de capas necesarias para la visualización de los
+	 * sexys tool tips
 	 */
-	for(var i=0;i<=n;i++){
-		$("#h"+i).tooltip (""+$('div.h'+i).html(), {
-		      width: 300,
-		      style: 'alert',		      
-		      hook: 1
-	    });
+	for ( var i = 0; i <= n; i++) {
+		$("#h" + i).tooltip("" + $('div.h' + i).html(), {
+			width : 300,
+			style : 'alert',
+			hook : 1
+		});
 	}
 });
 
@@ -41,108 +42,103 @@ $(document).ready(function(){
  * 
  * elem = objeto html img para uso del tool tip
  * 
- * $('div.'+elem.name).html() = Lee una capa oculta con los detalles del servicio incompleto
+ * $('div.'+elem.name).html() = Lee una capa oculta con los detalles del
+ * servicio incompleto
  * 
- * $("#"+elem.name).tooltip = es el identificado de la imagen la cual mostrarará los detalles 
- * del servicio incompleto.
+ * $("#"+elem.name).tooltip = es el identificado de la imagen la cual mostrarará
+ * los detalles del servicio incompleto.
  */
-function tip(elem){	
-	$("#"+elem.name).tooltip (""+$('div.'+elem.name).html(), {
-	      width: 250,
-	      style: 'alert',		      
-	      hook: 1
-    });
+function tip(elem) {
+	$("#" + elem.name).tooltip("" + $('div.' + elem.name).html(), {
+		width : 250,
+		style : 'alert',
+		hook : 1
+	});
 }
 
 /*
  * Función que muestra unos cuadro de dialogos hecho con el plugin sexy alert
  * que sirven para publicar un servicio de información.
  * 
- * parametros:
- * 		i = Objeto html del formulario
- * 		nombre = nombre del servicio de información a publicar 
+ * parametros: i = Objeto html del formulario nombre = nombre del servicio de
+ * información a publicar
  */
-function publicar_SI(i,nombre){
-	var action = 'id_pub_'+i;	
-	var formulario = document.getElementById(action);	
-	Sexy.confirm(data['mensajes']['publicar_servicio']+'<br>'+
-			'<br><h3>'+nombre+'</h3> <br>'+data['mensajes']['desea_continuar'], {
-	  onComplete:
-	    function(returnvalue) {
-	      if (returnvalue) {
-	    	  Sexy.info(data['mensajes']['el_servicio']+'<br><h3>'+nombre+'</h3> <br>'
-	    			  +data['mensajes']['ha_sido_publicado'], {
-	    		  onComplete:
-	    			  function(returnvalue) {
-	    			  formulario.submit();
-	    		  }
-    		  });
-	    		  
-	      } else {
-	    	  Sexy.alert(data['mensajes']['accion_cancelada']);
-	      }
-	    }
-	  });	
+function publicar_SI(i, nombre) {
+	var action = 'id_pub_' + i;
+	var formulario = document.getElementById(action);
+	Sexy.confirm(data['mensajes']['publicar_servicio'] + '<br>' + '<br><h3>'
+			+ nombre + '</h3> <br>' + data['mensajes']['desea_continuar'], {
+		onComplete : function(returnvalue) {
+			if (returnvalue) {
+				Sexy.info(data['mensajes']['el_servicio'] + '<br><h3>' + nombre
+						+ '</h3> <br>' + data['mensajes']['ha_sido_publicado'],
+						{
+							onComplete : function(returnvalue) {
+								formulario.submit();
+							}
+						});
+
+			} else {
+				Sexy.alert(data['mensajes']['accion_cancelada']);
+			}
+		}
+	});
 };
 
 /*
  * Función que muestra unos cuadro de dialogos hecho con el plugin sexy alert
  * que sirven para despublicar un servicio de información.
  * 
- * parametros:
- * 		i = Objeto html del formulario
- * 		nombre = nombre del servicio de información a despublicar 
+ * parametros: i = Objeto html del formulario nombre = nombre del servicio de
+ * información a despublicar
  */
-function despublicar_SI(i,nombre){
-	var action = 'id_despub_'+i;	
-	var formulario = document.getElementById(action);	
-	Sexy.confirm(data['mensajes']['despublicar_servicio']+'<br>'+
-			'<br><h3>'+nombre+'</h3> <br>'+data['mensajes']['desea_continuar'], {
-	  onComplete:
-	    function(returnvalue) {
-	      if (returnvalue) {
-	    	  Sexy.info(data['mensajes']['el_servicio']+'<br><h3>'+nombre+'</h3> <br>'
-	    			  +data['mensajes']['ha_sido_despublicado'], {
-	    		  onComplete:
-	    			  function(returnvalue) {
-	    			  formulario.submit();
-	    		  }
-    		  });
-	    		  
-	      } else {
-	    	  Sexy.alert(data['mensajes']['accion_cancelada']);
-	      }
-	    }
-	  });		
+function despublicar_SI(i, nombre) {
+	var action = 'id_despub_' + i;
+	var formulario = document.getElementById(action);
+	Sexy.confirm(data['mensajes']['despublicar_servicio'] + '<br>' + '<br><h3>'
+			+ nombre + '</h3> <br>' + data['mensajes']['desea_continuar'], {
+		onComplete : function(returnvalue) {
+			if (returnvalue) {
+				Sexy.info(data['mensajes']['el_servicio'] + '<br><h3>' + nombre
+						+ '</h3> <br>'
+						+ data['mensajes']['ha_sido_despublicado'], {
+					onComplete : function(returnvalue) {
+						formulario.submit();
+					}
+				});
+
+			} else {
+				Sexy.alert(data['mensajes']['accion_cancelada']);
+			}
+		}
+	});
 };
 
 /*
  * Función que muestra unos cuadro de dialogos hecho con el plugin sexy alert
  * que sirven para eliminar un servicio de información.
  * 
- * parametros:
- * 		i = Objeto html del formulario
- * 		nombre = nombre del servicio de información a despublicar 
+ * parametros: i = Objeto html del formulario nombre = nombre del servicio de
+ * información a despublicar
  */
-function eliminar_SI(i,nombre){
-	var action = 'id_'+i;	
-	var formulario = document.getElementById(action);	
-	Sexy.confirm(data['mensajes']['eliminar_servicio']+'<br>'+
-			'<br><h3>'+nombre+'</h3> <br>'+data['mensajes']['desea_continuar'], {
-	  onComplete:
-	    function(returnvalue) {
-	      if (returnvalue) {
-	    	  Sexy.info(data['mensajes']['el_servicio']+'<br><h3>'+nombre+'</h3> <br>'
-	    			  +data['mensajes']['ha_sido_eliminado'], {
-	    		  onComplete:
-	    			  function(returnvalue) {
-	    			  formulario.submit();
-	    		  }
-    		  });
-	    		  
-	      } else {
-	    	  Sexy.alert(data['mensajes']['accion_cancelada']);
-	      }
-	    }
-	  });				
+function eliminar_SI(i, nombre) {
+	var action = 'id_' + i;
+	var formulario = document.getElementById(action);
+	Sexy.confirm(data['mensajes']['eliminar_servicio'] + '<br>' + '<br><h3>'
+			+ nombre + '</h3> <br>' + data['mensajes']['desea_continuar'], {
+		onComplete : function(returnvalue) {
+			if (returnvalue) {
+				Sexy.info(data['mensajes']['el_servicio'] + '<br><h3>' + nombre
+						+ '</h3> <br>' + data['mensajes']['ha_sido_eliminado'],
+						{
+							onComplete : function(returnvalue) {
+								formulario.submit();
+							}
+						});
+
+			} else {
+				Sexy.alert(data['mensajes']['accion_cancelada']);
+			}
+		}
+	});
 };
