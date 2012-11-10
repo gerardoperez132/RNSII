d66ff963850bb2160de8a5274b319733cbb485e0
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-11-10 11:08:02 -0430

    Corrección de las descripciones en los comentarios y agregando formato de identación.

--- web/pages/res/js/registro/funcionalidades.js
+++ web/pages/res/js/registro/funcionalidades.js
@@ -1,74 +1,74 @@
-/*
- * Variable con las claves de intercionalización del archivo json. 
+/**
+ * Variable con las claves de internacionalización del arhcivo JSON.
  */
 var data;
 
-$(document).ready(function() {	
-	
-	/*
-	 * Obteniendo los valores de intercionalización del archivo JSON
+$(document).ready(function() {
+	/**
+	 * Obteniendo los valores de internacionalización del archivo JSON
 	 */
 	$.ajax({
-		url: "getJSONResult.action",		
-		type: "GET",
-		dataType: "json",
-		async:false,		
-		success: function(source){	
-			data = source;			
-		}	
+		url : "getJSONResult.action",
+		type : "GET",
+		dataType : "json",
+		async : false,
+		success : function(source) {
+			data = source;
+		}
 	});
-	
 });
 
-/* 
- * Función que muestra unos cuadro de dialogos hecho con el plugin sexy alert
- * que sirven para eliminar un servicio de información.
+/**
+ * Función que muestra unos cuadros de diálogo realizados con el plugin Sexy
+ * Alert que sirven para eliminar un servicio de información.
  * 
- * parametros:
- * 		i = Objeto html del formulario
- * 		nombre = nombre del servicio de información a despublicar 
+ * @param i
+ *            Objeto HTML del formulario.
+ * @param nombre
+ *            Nombre del servicio de información a despublicar.
  */
-function eliminar_Fun(i,nombre){
-	var action = 'id_'+i;	
-	var formulario = document.getElementById(action);	
-	Sexy.confirm(data['mensajes']['eliminar_funcionalidad']+'<br>'+
-			'<br><h3>'+nombre+'</h3> <br>'+data['mensajes']['desea_continuar'], {
-	  onComplete:
-	    function(returnvalue) {
-	      if (returnvalue) {
-	    	  Sexy.info(data['mensajes']['el_servicio']+'<br><h3>'+nombre+'</h3> <br>'
-	    			  +data['mensajes']['ha_sido_eliminado'], {
-	    		  onComplete:
-	    			  function(returnvalue) {
-	    			  formulario.submit();
-	    		  }
-    		  });
-	    		  
-	      } else {
-	    	  Sexy.alert(data['mensajes']['accion_cancelada']);
-	      }
-	    }
-	  });			
+function eliminar_Fun(i, nombre) {
+	var action = 'id_' + i;
+	var formulario = document.getElementById(action);
+	Sexy.confirm(data['mensajes']['eliminar_funcionalidad'] + '<br>'
+			+ '<br><h3>' + nombre + '</h3> <br>'
+			+ data['mensajes']['desea_continuar'], {
+		onComplete : function(returnvalue) {
+			if (returnvalue) {
+				Sexy.info(data['mensajes']['el_servicio'] + '<br><h3>' + nombre
+						+ '</h3> <br>' + data['mensajes']['ha_sido_eliminado'],
+						{
+							onComplete : function(returnvalue) {
+								formulario.submit();
+							}
+						});
+
+			} else {
+				Sexy.alert(data['mensajes']['accion_cancelada']);
+			}
+		}
+	});
 };
 
-/* 
- * Función que muestra unos cuadro de dialogos hecho con el plugin sexy alert
- * que sirven para abandonar la carga de datos para un servicio de información.
+/**
+ * Función que muestra unos cuadros de diálogo realizados con el plugin Sexy
+ * Alert que sirven para abandonar la carga de datos para un servicio de
+ * información.
  * 
- * parametros:
- * 		nombre = nombre del servicio de información 
+ * @param nombre
+ *            Nombre del servicio de información
  */
-function salir(nombre){	
-	var formulario = document.getElementById('salir');	
-	Sexy.confirm(data['mensajes']['abandonar_carga_datos_si']+' <br>'+
-			'<br><h3>'+nombre+'</h3> <br>'+data['mensajes']['desea_continuar'], {
-	  onComplete:
-	    function(returnvalue) {
-	      if (returnvalue) {	    	  
-	    	  formulario.submit();
-	      } else {
-	    	  Sexy.alert(data['mensajes']['accion_cancelada']);
-	      }
-	    }
-	  });			
+function salir(nombre) {
+	var formulario = document.getElementById('salir');
+	Sexy.confirm(data['mensajes']['abandonar_carga_datos_si'] + ' <br>'
+			+ '<br><h3>' + nombre + '</h3> <br>'
+			+ data['mensajes']['desea_continuar'], {
+		onComplete : function(returnvalue) {
+			if (returnvalue) {
+				formulario.submit();
+			} else {
+				Sexy.alert(data['mensajes']['accion_cancelada']);
+			}
+		}
+	});
 };
